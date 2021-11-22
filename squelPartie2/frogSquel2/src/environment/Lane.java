9c95c27afe236eade601a75159a59a104214c17c
mitokpe <93189052+mitokpe@users.noreply.github.com>
2021-11-22 17:20:27 +0100

    Update Lane.java

--- squelPartie2/frogSquel2/src/environment/Lane.java
+++ squelPartie2/frogSquel2/src/environment/Lane.java
@@ -14,31 +14,61 @@ public class Lane {
 	private double density;
 
 	// TODO : Constructeur(s)
+	
+	public Lane (Game game) {
+		this.game = game;
+		this.ord = ord;
+		this.speed = speed;
+		this.cars = cars;
+		this.leftToRight = leftToRight;
+		this.density = density;
+	}
 
 	public void update() {
 
 		// TODO
 
-		// Toutes les voitures se déplacent d'une case au bout d'un nombre "tic
-		// d'horloge" égal à leur vitesse
-		// Notez que cette méthode est appelée à chaque tic d'horloge
+		// Toutes les voitures se dÃ©placent d'une case au bout d'un nombre "tic
+		// d'horloge" Ã©gal Ã  leur vitesse
+		// Notez que cette mÃ©thode est appelÃ©e Ã  chaque tic d'horloge
 
 		// Les voitures doivent etre ajoutes a l interface graphique meme quand
 		// elle ne bougent pas
 
-		// A chaque tic d'horloge, une voiture peut être ajoutée
+		// A chaque tic d'horloge, une voiture peut Ãªtre ajoutÃ©e
 
 	}
 
 	// TODO : ajout de methodes
+	
+	/**
+	 * avoir la vitesse
+	 */
+	public int getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * avoir la direction
+	 */
+	public boolean getDirection() {
+		return leftToRight;
+	}
+
+	/**
+	 * avoir la densite
+	 */
+	public int getDensity() {
+		return density;
+	}
 
 	/*
 	 * Fourni : mayAddCar(), getFirstCase() et getBeforeFirstCase() 
 	 */
 
 	/**
-	 * Ajoute une voiture au début de la voie avec probabilité égale à la
-	 * densité, si la première case de la voie est vide
+	 * Ajoute une voiture au dÃ©but de la voie avec probabilitÃ© Ã©gale Ã  la
+	 * densitÃ©, si la premiÃ¨re case de la voie est vide
 	 */
 	private void mayAddCar() {
 		if (isSafe(getFirstCase()) && isSafe(getBeforeFirstCase())) {
