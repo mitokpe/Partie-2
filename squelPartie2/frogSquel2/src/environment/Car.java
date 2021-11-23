426d9d47d1fd321509ac8ce338facfcf0042cb8e
mitokpe <abdessalem.bouzidi@universite-paris-saclay.fr>
2021-11-23 11:50:10 +0100

    corriger les erreurs

--- squelPartie2/frogSquel2/src/environment/Car.java
+++ squelPartie2/frogSquel2/src/environment/Car.java
@@ -1,10 +1,10 @@
 package environment;
 
-import java.awt.Color;
-
-import gameCommons.Case;
+import util.Case;
 import gameCommons.Game;
 import graphicalElements.Element;
+
+import java.awt.*;
 
 public class Car {
 	private Game game;
