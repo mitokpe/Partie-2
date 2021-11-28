b16b16809482a84eba75a0bebdd76d9b3cad5054
mitokpe <93189052+mitokpe@users.noreply.github.com>
2021-11-28 12:35:28 +0100

    Add files via upload

---
+++ squelPartie2/frog/Frog.java
@@ -0,0 +1,46 @@
+package frog;
+
+import gameCommons.*;
+import util.*;
+
+public class Frog implements IFrog {
+	
+	private Game game;
+	private Direction direction;
+	private Case cases;
+
+	public Frog(Game game){
+		this.game = game;
+		this.direction = Direction.up;
+		this.cases = new Case (this.game.width / 2,  0);
+	}
+
+	public Case getPosition() { return this.cases; }
+
+	public Direction getDirection() { return this.direction; }
+
+	public void move(Direction key) {
+		this.direction = key;
+
+		if (key == Direction.up && this.cases.ord + 1 < this.game.height) {
+			this.cases = new Case (this.cases.absc,  this.cases.ord + 1);
+		}
+
+		if (key == Direction.down && this.cases.ord  > 0) {
+			this.cases = new Case (this.cases.absc, this.cases.ord - 1);
+		}
+
+		if (key == Direction.left && this.cases.absc  > 0) {
+			this.cases = new Case (this.cases.absc - 1, this.cases.ord);
+		}
+
+		if (key == Direction.right && this.cases.absc + 1 < this.game.width) {
+			this.cases = new Case (this.cases.absc + 1, this.cases.ord);
+		}
+
+		// this.game.testWin()
+		this.game.testLose();
+	}
+
+
+}
