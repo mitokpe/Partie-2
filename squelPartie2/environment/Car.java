e92c43206f7fa5c971ac1a854a5c534bb69b6ef5
mitokpe <abdessalem.bouzidi@universite-paris-saclay.fr>
2021-11-30 12:46:09 +0100

    faire les liaisons

---
+++ squelPartie2/environment/Car.java
@@ -0,0 +1,62 @@
+package environment;
+
+import util.Case;
+import gameCommons.Game;
+import graphicalElements.Element;
+
+import java.awt.*;
+
+public class Car {
+	private Game game;
+	private Case leftPosition;
+	private boolean leftToRight;
+	private int length;
+	private Color colorLtR = Color.BLACK;
+	private Color colorRtL = Color.BLUE;
+
+	//TODO Constructeur(s)
+	
+	public Car(Game game, Case firstCase, Boolean leftToRight) {
+		this.game = game;
+		this.leftPosition = new Case(leftToRight ? firstCase.absc - this.length : firstCase.absc, firstCase.ord);
+		this.leftToRight = leftToRight;
+		this.length = length;
+		this.colorLtR = Color.BLACK;
+		this.colorRtL = Color.BLUE;
+	}
+	
+	//TODO : ajout de methodes
+
+	public void move(boolean b) {
+		if (b) {
+			this.leftPosition = new Case(leftToRight ? this.leftPosition.absc + 1 : this.leftPosition.absc + 1 , this.leftPosition.ord);
+		}
+
+		this.addToGraphics();
+	}
+
+	public boolean dansLaVoie () {
+		return this.leftPosition.absc + this.length > 0 && this.leftPosition.absc < this.game.width;
+	}
+
+	public boolean verification (Case case1) {
+		if (case1.ord != this.leftPosition.ord) {
+			return false;
+		} else {
+			return case1.absc >= this.leftPosition.absc && case1.absc < this.leftPosition.absc + this.length;
+		}
+	}
+	
+	/* Fourni : addToGraphics() permettant d'ajouter un element graphique correspondant a la voiture*/
+	private void addToGraphics() {
+		for (int i = 0; i < length; i++) {
+			Color color = colorRtL;
+			if (this.leftToRight){
+				color = colorLtR;
+			}
+			game.getGraphic()
+					.add(new Element(leftPosition.absc + i, leftPosition.ord, color));
+		}
+	}
+
+}
---
+++ squelPartie2/environment/Environment.java
@@ -0,0 +1,52 @@
+package environment;
+
+import gameCommons .Game;
+import gameCommons.IEnvironment;
+import givenEnvironment.Lane;
+import util.Case;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public abstract class Environment implements IEnvironment {
+		
+	//TODO
+
+    public class environment implements IEnvironment {
+        private ArrayList<Lane> road;
+        private Game game;
+
+        public environment (Game game) {
+            this.game = game;
+            this.road = new ArrayList();
+            this.road.add(new Lane(game, 0, 0.0D));
+
+            for(int i = 1; i < game.height - 1; ++i) {
+                this.road.add(new Lane(game, i));
+            }
+
+            this.road.add(new Lane(game, game.height - 1, 0.0D));
+        }
+
+    @Override
+    public boolean isSafe(Case c) {
+        return ((Lane)this.road.get(c.ord)).isSafe(c);
+    }
+
+    @Override
+    public boolean isWinningPosition(Case c) {
+        return c.ord == this.game.height - 1;
+        }
+
+    @Override
+    public void update() {
+        Iterator line = this.road.iterator();
+
+        while(line.hasNext()) {
+            Lane lane = (Lane)line.next();
+            lane.update();
+        }
+
+    }
+    }
+}
